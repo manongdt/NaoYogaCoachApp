e874f1af9841af8051a4082916bf559cf08245e8
AlexandraDuport <alexandra.duport@epfedu.fr>
2015-11-22 15:04:12 +0100

    Code sans bug du xml de l'heure

--- src/de/robotik/nao/communicator/core/sections/SectionAlarm.java
+++ src/de/robotik/nao/communicator/core/sections/SectionAlarm.java
@@ -4,9 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TimePicker;
+
+
 import de.robotik.nao.communicator.R;
 
 public class SectionAlarm extends Section {
+//définition des TimePicker 	
+private TimePicker timePickerAlarm;
+//int hour = timePickerAlarm.getCurrentHour();
+//int min = timePickerAlarm.getCurrentMinute();
+
 
 	public SectionAlarm(String aTitle) {
 		super(aTitle);
@@ -16,8 +24,9 @@ public class SectionAlarm extends Section {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		// connect ui widgets
 				rootView = inflater.inflate(R.layout.page_alarm, container, false);
-		//get widget...
-				
+		//get widget
+		//timePicker		
+		timePickerAlarm = (TimePicker) findViewById(R.id.timePickerAlarm);		
 				return rootView;
 	}
 
