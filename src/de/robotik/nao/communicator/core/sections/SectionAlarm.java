52dfac0ffe8431b977c4400b8aad5861fb0dfe31
AlexandraDuport <alexandra.duport@epfedu.fr>
2015-12-01 14:40:08 +0100

    qui sert a rien

--- src/de/robotik/nao/communicator/core/sections/SectionAlarm.java
+++ src/de/robotik/nao/communicator/core/sections/SectionAlarm.java
@@ -1,19 +1,31 @@
 package de.robotik.nao.communicator.core.sections;
 
+import java.util.Calendar;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
-
-
+import android.widget.Toast;
 import de.robotik.nao.communicator.R;
 
 public class SectionAlarm extends Section {
 //définition des TimePicker 	
 private TimePicker timePickerAlarm;
-//int hour = timePickerAlarm.getCurrentHour();
-//int min = timePickerAlarm.getCurrentMinute();
+private int hour, min;
+
+//date picker
+private DatePicker datePickerAlarm;
+private Calendar calendar;
+private int year, month, day;
+
+//bouton
+private Button btn;
 
 
 	public SectionAlarm(String aTitle) {
@@ -26,7 +38,36 @@ private TimePicker timePickerAlarm;
 				rootView = inflater.inflate(R.layout.page_alarm, container, false);
 		//get widget
 		//timePicker		
-		timePickerAlarm = (TimePicker) findViewById(R.id.timePickerAlarm);		
+		timePickerAlarm = (TimePicker) findViewById(R.id.timePickerAlarm);	
+		//datePicker
+		datePickerAlarm = (DatePicker) findViewById(R.id.datePickerAlarm);	
+		//button
+		btn = (Button) findViewById(R.id.okButton);
+	//onclick.......................................................	
+		btn.setOnClickListener( new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+//get time				
+ hour = timePickerAlarm.getCurrentHour();
+ min = timePickerAlarm.getCurrentMinute();
+
+//get date
+ calendar = Calendar.getInstance();
+ year = calendar.get(Calendar.YEAR);
+ 
+ month = calendar.get(Calendar.MONTH);
+ day = calendar.get(Calendar.DAY_OF_MONTH);
+ 
+ /*Context context = getActivity().getApplicationContext();
+ int text = hour;
+ int duration = Toast.LENGTH_SHORT;
+
+ Toast toast = Toast.makeText(context, text, duration);
+ toast.show();*/			}
+		} );
+		
+
+//................................................................................		
 				return rootView;
 	}
 
