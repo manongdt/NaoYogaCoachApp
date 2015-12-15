bbb35cd7a369b4a8b6d5fab68b427532d42423c9
AlexandraDuport <alexandra.duport@epfedu.fr>
2015-12-15 15:38:41 +0100

    Revert "pour un ptit revert"
    This reverts commit efd0609862ea89023d37c2509a9c618bc4e627f3.

--- src/de/robotik/nao/communicator/core/sections/SectionYoga.java
+++ src/de/robotik/nao/communicator/core/sections/SectionYoga.java
@@ -5,12 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageButton;
 import de.robotik.nao.communicator.R;
+import de.robotik.nao.communicator.core.RemoteNAO;
 import de.robotik.nao.communicator.network.data.NAOCommands;
 
 public class SectionYoga extends Section implements OnClickListener{
-ImageButton btnYoga;
+private Button btnYoga;
 
 	public SectionYoga(String nomPage) {
 		// TODO Auto-generated constructor stub
@@ -27,7 +29,7 @@ ImageButton btnYoga;
 		View rootView = inflater.inflate(R.layout.page_yoga, container, false);
 
 		//get view ................................................................................
-		btnYoga = (ImageButton) findViewById(R.id.btnYoga);
+		btnYoga = (Button) findViewById(R.id.btnYoga);
 		// set listener
 		btnYoga.setOnClickListener(this);
 		return rootView;
@@ -37,16 +39,13 @@ ImageButton btnYoga;
 	public void onClick(View v) {
 		if (v.getId()==R.id.btnYoga){
 			playProgram();
-		
+		}
 	}
 	
-	
+
 	private void playProgram(){
 	// send command
 	RemoteNAO.sendCommand(NAOCommands.PLAY_YOGA);		
 	}
-	@Override
 	
-
-}
 }
